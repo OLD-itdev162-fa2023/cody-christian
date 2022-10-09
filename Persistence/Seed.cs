e423079630f50e4e034fd0ee9121a8d9260562e8
Cody Christian <codychristian@Codys-Air.attlocal.net>
2022-10-09 13:40:38 -0500

    Created Posts Controller

--- Persistence/Seed.cs
+++ Persistence/Seed.cs
@@ -2,11 +2,38 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Domain;
 
 namespace Persistence
 {
     public class Seed
     {
-        
+        public static void SeedData (DataContext context)
+        {
+            if (!context.Posts.Any())
+            {
+                var Posts = new List<Post>
+                {
+                    new Post {
+                        Title = "First Post",
+                        Body = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                        Date = DateTime.Now.AddDays(-10)
+                    },
+                    new Post {
+                        Title = "Second Post",
+                        Body = "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                        Date = DateTime.Now.AddDays(-7)
+                    },
+                    new Post {
+                        Title = "Third Post",
+                        Body = "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+                        Date = DateTime.Now.AddDays(-4)
+                    }
+                };
+
+                context.Posts.AddRange(Posts);
+                context.SaveChanges();
+            }
+        }
     }
 }
