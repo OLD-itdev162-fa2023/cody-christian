1e9dc2e3f83f3e4799c1aafa58e803817fb55117
Cody Christian <codychristian@Codys-Air.attlocal.net>
2022-10-09 15:18:35 -0500

    Completed Activity 5

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -8,7 +8,7 @@ using Microsoft.EntityFrameworkCore;
 namespace Persistence
 {
     public class DataContext: DbContext
-    {
+    {      
         public DbSet<WeatherForecast> WeatherForecasts {get; set;}
         public DbSet<Post> Posts { get; set; }
         public string DbPath {get;}
