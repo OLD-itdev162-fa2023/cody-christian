a602a6fe0c765b2c236ccfce71e307513a7c19be
Cody Christian <codychristian@Codys-Air.attlocal.net>
2022-10-25 20:35:16 -0500

    Create GetById and Posts endpoints

--- API/Controllers/PostsController.cs
+++ API/Controllers/PostsController.cs
@@ -19,10 +19,60 @@ namespace API.Controllers
             this.context = context;
         }
 
+        /// <summary>
+        /// GET api/posts
+        /// </summary>
+        /// <returns>A list of posts</returns>
         [HttpGet(Name = "GetPosts")]
         public ActionResult<List<Post>> Get()
         {
             return this.context.Posts.ToList();
+        }
+
+
+        /// <summary>
+        /// GET api/post/[id]
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>A single post</returns>
+        [HttpGet("{id}", Name = "GetById")]
+        public ActionResult<Post> GetById(Guid id)
+        {
+            var post = this.context.Posts.Find(id);
+            if (post is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(post);
+        }
+
+        /// <summary>
+        /// POST api/post
+        /// </summary>
+        /// <param name="request">JSON request containing post fields</param>
+        /// <returns>A new post</returns>
+        [HttpPost(Name = "Create")]
+        public ActionResult<Post> Create([FromBody]Post request)
+        {
+            var post = new Post
+            {
+                Id = request.Id,
+                Title = request.Title,
+                Body= request.Body,
+                Date = request.Date
+            };
+
+            context.Posts.Add(post);
+            var success = context.SaveChanges() > 0;
+
+            if (success)
+            {
+                return Ok(post);
+
+            }
+
+            throw new Exception("Error creating post");
         }
     }
 }
